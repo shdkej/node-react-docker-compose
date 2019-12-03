b5c0d663c800f035ac4406b335108163358c6a9f
shdkej <shdkej@gmail.com>
2019-12-03 21:36:09 +0900

    add Square

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './compiled/App.css';
+import Square from './Square.js';
+//import './App.scss';
 
 class App extends Component {
   constructor() {
@@ -40,13 +41,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{this.state.message || 'No message'}</p>
+        <Square/>
       </div>
     );
   }
